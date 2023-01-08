88028bb3982a1c1430f104d8f6dba2cd79cc0d09
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2023-01-08 11:02:27 +0100

    chore: rename variables for production

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/dynamic/public';
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabaseClient = createClient(supabaseUrl, supabaseKey);
