62dfbbf59f1974c801cbe9aeeebe0240eee785cb
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2023-08-15 11:23:43 +0200

    feat: add Torstens story

---
+++ src/routes/torstens-levnadsbeskrivning/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import Auth from '$lib/Auth.svelte';
+	import Torsten from '$lib/Torsten.svelte';
+
+	onMount(() => {
+		if (!$page.data.session) {
+			window.location.href = '/#';
+		}
+	});
+</script>
+
+<svelte:head>
+	<title>Torpet - Torstens levnadsbeskrivning</title>
+	<meta name="description" content="SvelteKit using supabase-js v2" />
+</svelte:head>
+
+{#if !$page.data.session}
+	<Auth />
+{:else}
+	<Torsten />
+{/if}
