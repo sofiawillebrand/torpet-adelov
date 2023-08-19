018cf6457388a916522e96cf33920e27a264fa0f
Sofia Willebrand <82705594+sofiawillebrand@users.noreply.github.com>
2023-08-19 14:39:11 +0200

    Feature/hamburger (#4)
    * feat: add hamburger button
    
    * feat: move nav-links to hamburger container

---
+++ src/lib/HamburgerMenu/HamburgerButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let isOpen = true;
+	const handleButtonClick = () => {
+		isOpen = !isOpen;
+	};
+</script>
+
+<button class="space-y-2 z-20" on:click={handleButtonClick}>
+	<div
+		class:rotate-45={isOpen}
+		class:translate-y-2={isOpen}
+		class="w-8 h-0.5 bg-sky-800 transform transition-all"
+	/>
+	<div class:opacity-0={isOpen} class="w-8 h-0.5 bg-sky-800 transform transition-all" />
+	<div
+		class:-rotate-45={isOpen}
+		class:-translate-y-3={isOpen}
+		class="w-8 h-0.5 bg-sky-800 transform transition-all"
+	/>
+</button>
---
+++ src/lib/HamburgerMenu/HamburgerMenu.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let isOpen = false;
+
+	$: onRoute = $page.route.id;
+</script>
+
+<div class=" fixed bottom-0 right-0 left-0 top-0 bg-sky-500 z-10">
+	<div class="flex flex-col mx-auto md:w-2/4">
+		<div class="flex items-center flex-shrink-0 text-white p-6">
+			<span class="font-semibold text-xl tracking-tight">Torpet</span>
+		</div>
+		<a
+			on:click={() => (isOpen = false)}
+			href="/"
+			class="mt-4 ml-6 inline-block lg:mt-0 hover:text-white mr-2 
+      {onRoute === '/' ? 'font-bold text-white' : 'text-teal-200'}"
+		>
+			Info
+		</a>
+		{#if $page.data.session}
+			<a
+				on:click={() => (isOpen = false)}
+				href="/kalender"
+				class="mt-4 ml-6 inline-block lg:mt-0 hover:text-white mr-2
+      {onRoute === '/kalender' ? 'font-bold text-white' : 'text-teal-200'}"
+			>
+				Kalender
+			</a>
+			<a
+				on:click={() => (isOpen = false)}
+				href="/mina-bokningar"
+				class="mt-4 ml-6 inline-block lg:mt-0  hover:text-white mr-2
+    {onRoute === '/mina-bokningar' ? 'font-bold text-white' : 'text-teal-200'}"
+			>
+				Mina bokningar
+			</a>
+			<a
+				on:click={() => (isOpen = false)}
+				href="/torstens-levnadsbeskrivning"
+				class="mt-4 ml-6 inline-block lg:mt-0  hover:text-white
+    {onRoute === '/torstens-levnadsbeskrivning' ? 'font-bold text-white' : 'text-teal-200'}"
+			>
+				Torstens Levnadsbeskrivning
+			</a>
+			<a
+				on:click={() => (isOpen = false)}
+				href="/mina-uppgifter"
+				class="mt-4 ml-6 inline-block lg:mt-0  hover:text-white
+      {onRoute === '/mina-uppgifter' ? 'font-bold text-white' : 'text-teal-200'}"
+			>
+				Mina uppgifter
+			</a>
+		{/if}
+	</div>
+</div>
